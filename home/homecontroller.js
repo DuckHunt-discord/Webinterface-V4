305065d5cdce43735097f83956296b160a6901d8
Arthur Jovart <arthur@jovart.com>
2020-12-05 17:45:31 +0100

    Add a few columns

--- home/homecontroller.js
+++ home/homecontroller.js
@@ -86,7 +86,7 @@ myApp.controller('HomeController', ["$scope", "$state", "$http", "sampleFactory"
   
           // this callback will be called asynchronously
           // when the response is available
-          console.log('get ok');
+          console.log('get1 ok');
           $scope.players = response.data;
           console.log(response.data)
       }, function errorCallback(response) {
@@ -94,7 +94,28 @@ myApp.controller('HomeController', ["$scope", "$state", "$http", "sampleFactory"
    
           // called asynchronously if an error occurs
           // or server returns response with an error status.
-          console.log('get nok');
+          console.log('get1 nok');
+      });
+
+      $http({
+        method: 'GET',
+        url: 'https://f0dc2044b4e6.ngrok.io/api/channels/734880436789968998',
+
+      }).then(function successCallback(response) {
+       // response.setHeader("Access-Control-Allow-Origin", "*");
+
+
+          // this callback will be called asynchronously
+          // when the response is available
+          console.log('get2 ok');
+          $scope.channel_info = response.data;
+          console.log(response.data)
+      }, function errorCallback(response) {
+       // response.setHeader("Access-Control-Allow-Origin", "*");
+
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+          console.log('get2 nok');
       });
     
   }
